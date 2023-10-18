958e3e4e284bff93623241a910c703d75c910382
Yashin <yashinnaran@gmail.com>
2023-10-18 14:45:41 +0200

    Removed redundant div from TipDisplay.js , set ResetButton to have a margin top of auto.

--- src/components/TipDisplay/TipDisplay.js
+++ src/components/TipDisplay/TipDisplay.js
@@ -4,16 +4,15 @@ import ResetButton from "./ResetButton";
 const TipDisplay = () => {
   return (
     <div className={styles.display_container}>
-      <div className={""}>
-        <div className={styles.results}>
-          <div>
-            <p className={styles.result_name}>tip amount</p>
-            <p className={styles.result_subscript}>/ person</p>
-          </div>
-
-          <p className={styles.amount_display}>$0.00</p>
+      <div className={styles.results}>
+        <div>
+          <p className={styles.result_name}>tip amount</p>
+          <p className={styles.result_subscript}>/ person</p>
         </div>
+
+        <p className={styles.amount_display}>$0.00</p>
       </div>
+
       <ResetButton />
     </div>
   );
